a0045b102852d9c9e3bb411307c90a6bb8303f84
Sotatek-SangDo <sang.do@sotatek.com>
2018-12-03 18:35:30 +0700

    user skill

--- src/components/employee/AddSkills.vue
+++ src/components/employee/AddSkills.vue
@@ -7,12 +7,22 @@
         </a>
       </div>
       <div class="collapse show" data-parent="#accordion6">
-        <div class="card-body">
-          content
+        <div class="card-body" v-if="userSkills">
+          <div class="list-group-item sub-tab-item" v-for="(s, i) in userSkills" :key="i">
+            <h5 class="list-group-item-heading">{{ s.skill}}
+              <button class="but but-del" type="button" tooltip="Delete" @click="removeSkill(s)">
+                <i class="ti-trash"></i>
+              </button>
+              <button class="but but-edit" type="button" tooltip="Edit" @click="showModalUpdate(s)">
+                <i class="ti-marker-alt"></i>
+              </button>
+            </h5>
+            <p class="list-group-item-text">{{ s.detail }}</p>
+          </div>
         </div>
       </div>
     </div>
-    <div class="modal fade show" id="add-skill">
+    <div class="modal fade show" ref="add_modal" id="add-skill">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -24,7 +34,7 @@
               <div class="col-12 mt-12">
                 <div class="card">
                   <div class="card-body">
-                    <form @submit-prevent="store">
+                    <form @submit.prevent="store">
                       <div class="form-group">
                         <label for="kni">Kĩ năng</label>
                         <select v-model="user_skill.skill_id" id="kni" class="form-control">
@@ -42,7 +52,7 @@
                       <div v-if="hasErrors()" class="errors">
                         <span v-text="errors[0].keys"/>
                       </div>
-                      <button type="submit" :disabled="isDisable" class="btn btn-primary mt-4 pr-4 pl-4">Lưu</button>
+                      <button type="submit" :disabled="isDisable" class="btn btn-primary mt-4 pr-4 pl-4"><i class="ti-save"></i> Lưu</button>
                     </form>
                   </div>
                 </div>
@@ -58,6 +68,7 @@
 <script>
 import rf from "../../requests/RequestFactory";
 import MasterView from "../../views/MasterView";
+import $ from "jquery";
 import _ from "lodash";
 
 export default {
@@ -69,10 +80,12 @@ export default {
       user_skill: {
         skill_id: "",
         detail: "",
-        user_id: this.empId
+        emp_id: this.empId,
+        id: ""
       },
       errors: [],
-      isDisable: false
+      isDisable: false,
+      userSkills: []
     };
   },
   props: {
@@ -86,6 +99,11 @@ export default {
         .getAll()
         .then(res => (this.skills = res));
     },
+    getUserSkill() {
+      rf.getRequest("SkillUserRequest")
+        .getAll()
+        .then(res => (this.userSkills = res));
+    },
     hasErrors() {
       return !_.isEmpty(this.errors);
     },
@@ -97,18 +115,66 @@ export default {
         if (!val && keyNullable.indexOf(key) === -1)
           this.errors.push({ keys: `${key} yêu cầu, không được rỗng.` });
       });
-      if (this.errors) {
+      if (this.errors.length) {
         this.isDisable = false;
         return;
       }
+      if (this.user_skill.id) {
+        return rf
+          .getRequest("SkillUserRequest")
+          .update(this.user_skill)
+          .then(res => {
+            if (res.status) {
+              const index = this.userSkills.findIndex(
+                s => s.id === this.user_skill.id
+              );
+              this.userSkills[index] = res.data;
+              this.clearData();
+              $(this.$refs.add_modal).modal("hide");
+            }
+          });
+      }
       rf.getRequest("SkillUserRequest")
         .store(this.user_skill)
-        .then(res => (this.errors = res));
+        .then(res => {
+          if (res.status) {
+            this.clearData();
+            $(this.$refs.add_modal).modal("hide");
+            this.userSkills.push(res.data);
+          }
+        });
+    },
+    showModalUpdate(skill) {
+      this.user_skill.skill_id = skill.skill_id;
+      this.user_skill.detail = skill.detail;
+      this.user_skill.id = skill.id;
+      $(this.$refs.add_modal).modal("show");
+    },
+    removeSkill(skill) {
+      rf.getRequest("SkillUserRequest")
+        .destroy({ id: skill.id })
+        .then(res => {
+          if (res.status) {
+            this.userSkills.splice(this.userSkills.indexOf(skill), 1);
+          }
+        });
+    },
+    clearData() {
+      this.user_skill.skill_id = "";
+      this.user_skill.detail = "";
+      this.user_skill.id = "";
+      this.isDisable = false;
     },
     init() {
       this.getSkill();
-    },
-    addSkill() {}
+      this.getUserSkill();
+      $(this.$refs.add_modal).on(
+        "hidden.bs.modal",
+        function() {
+          this.clearData();
+        }.bind(this)
+      );
+    }
   },
   mounted() {
     this.init();
@@ -136,4 +202,21 @@ a.icon-p
     font-weight: 900
     padding-left: 4px
     margin-right: 30px
+.but
+  border: 1px solid #bfbfbf
+  color: blue
+  font-size: 20px
+  background-color: transparent
+  position: absolute
+  top: 0
+  padding: 5px
+  &.but-del
+    right: 0
+    border-right: none
+    border-left: none
+    border-top: none
+  &.but-edit
+    right: 30px
+    border-top: none
+    border-radius: 0 0 0 5px
 </style>
--- src/requests/SkillUserRequest.js
+++ src/requests/SkillUserRequest.js
@@ -9,6 +9,14 @@ export default class SkillUserRequest extends BaseRequest {
     const url = this.parseUrl("/store");
     return this.post(url, params);
   }
+  update(params) {
+    const url = this.parseUrl("/update");
+    return this.post(url, params);
+  }
+  destroy(params) {
+    const url = this.parseUrl("/destroy");
+    return this.post(url, params);
+  }
   getModel() {
     return "/skill-user";
   }
