ad683188504d1e11176b303e47c80ae2a2865ec7
ThanhDu <thanh.du@sotatek.com>
2019-01-25 14:16:26 +0700

    mock login

--- src/api/login.js
+++ src/api/login.js
@@ -3,6 +3,12 @@ import Oauth2 from '@/auth/OAuth'
 
 export function loginByUsername(userInfo) {
   return Oauth2.loginByUserPass(userInfo)
+
+  // Using this mock if you don't have network
+  // return request({
+  //   url: '/login/login',
+  //   method: 'post'
+  // })
 }
 
 export function logout() {
@@ -14,5 +20,11 @@ export function logout() {
 
 export function getUserInfo() {
   return Oauth2.getUserInfo()
+
+  // Using this mock if you don't have network
+  // return request({
+  //   url: '/user/info',
+  //   method: 'get'
+  // })
 }
 
